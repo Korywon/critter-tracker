fc529a21c45864dea7860265ddef5252dccbee0b
Korywon <22848944+Korywon@users.noreply.github.com>
2020-03-31 23:50:56 -0500

    Add FontAwesome brands and add GitHub link to about component

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 export default class About extends Component {
   render () {
     return (
@@ -8,6 +11,10 @@ export default class About extends Component {
           <p>
             About under construction!
           </p>
+          <a href="https://www.github.com/korywon/critter-tracker"
+            rel="noopener noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faGithub} size="6x" />
+          </a>
         </div>
       </div>
     );
