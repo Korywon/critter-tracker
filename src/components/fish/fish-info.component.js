bd433d748b850008a78142ef7915af9e7b4492b9
Korywon <22848944+Korywon@users.noreply.github.com>
2020-04-15 00:00:27 -0500

    Implement basic layout of fish info component

--- src/components/fish/fish-info.component.js
+++ src/components/fish/fish-info.component.js
@@ -1,50 +1,141 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Media } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
+
+import MonthUtility from '../../utility/month-utility';
+import MonthBar from '../month-bar.component';
 
 import BreadcrumbMenu from '../breadcrumb.component';
+import StatusBadges from '../status-badges.component';
+
+const Status = props => {
+  let northMonths = [];
+  let southMonths = [];
+
+  if (props.fish.hasOwnProperty('months')) {
+    if (props.fish.months.hasOwnProperty('north')) {
+      northMonths = props.fish.months.north;
+    }
+    if (props.fish.months.hasOwnProperty('south')) {
+      southMonths = props.fish.months.south;
+    }
+  }
+
+  return (
+    <>
+      <h6>Northern Hemisphere</h6>
+      <StatusBadges
+        status={{
+          status: props.fish.status.north,
+          id: props.fish.id + '-north'}}
+        key={props.fish.id + '-hemisphereStatus-north'} />
+      <br />
+      <br />
+      <MonthBar timeSpans={northMonths} />
+      <br />
+      <h6>Southern Hemisphere</h6>
+      <StatusBadges
+        status={{
+          status: props.fish.status.south,
+          id: props.fish.id + '-south'}}
+        key={props.fish.id + '-hemisphereStatus-south'} />
+      <br />
+      <br />
+      <MonthBar timeSpans={southMonths} />
+    </>
+  );
+}
+
+/**
+ * TODO: Implement the time badges. Might need to create components for the
+ * time badges before anything.
+ * @param {*} props 
+ */
+const Time = props => {
+  return (
+    <>
+
+    </>
+  );
+}
 
 const Fish = props => {
-  return (
-    <div>
-      <Media>
-        <img 
-          width={100}
-          height={100}
-          className="mr-3"
-          src={"/image/fish/" + props.fish.id + ".webp"}
-          alt={props.fish.name}
-        />
-        <Media.Body>
-          <h4>{props.fish.name}</h4>
+
+  if (Object.keys(props.fish).length > 0) {
+    return (
+      <Card>
+        <Card.Body>
+          <Container className="text-center">
+            <img 
+              width={50}
+              height={50}
+              className="mr-3"
+              src={"/image/fish/" + props.fish.id + ".webp"}
+              alt={props.fish.name}
+            />
+            <Card.Title>
+              {props.fish.name}
+            </Card.Title>
+            <hr />
+            <Status fish={props.fish} />
+            <hr />
+          </Container>
           <p>{props.fish.location}</p>
-          <p>{props.fish.price}</p>
-          <p>-</p>
-          <p>-</p>
-          <p>-</p>
-        </Media.Body>
-      </Media>
-    </div>
-  );
+          <p>
+            <img 
+              width={25} height={25} src="/image/bell_bag.png"
+              alt="bell_bag.png"/>&nbsp;
+            {props.fish.price}
+          </p>
+          <Time fish={props.fish} />
+        </Card.Body>
+      </Card>
+    );
+  } else {
+    return(<></>);
+  }
 }
 
 export default class FishInfo extends Component {
   constructor (props) {
     super(props);
 
-    this.state = { fish: {} };
+    this.state = {
+      fish: {
+        name: null,
+        location: null,
+        size: null,
+        price: null,
+        months: {
+          north: [],
+          south: []
+        },
+        time: [],
+        status: {
+          north: {},
+          south: {}
+        }
+      } 
+    };
+  }
+
+  fishInfo () {
+    return <Fish fish={this.state.fish} key={this.state.fish.id} />
   }
 
   render () {
     return (
-      <div className="container">
+      <Container>
         <div className="text-center">
           Fish info under construction!
         </div>
         <br />
         <BreadcrumbMenu />
-        { this.fishInfo() }
-      </div>
+        <Container
+          style={{paddingLeft: "10rem", paddingRight: "10rem"}}>
+          { this.fishInfo() }
+        </Container> 
+      </Container>
     );
   }
 
@@ -59,17 +150,25 @@ export default class FishInfo extends Component {
           x => x.name.replace(/ /g, '_') === this.props.match.params.name
         );
 
+        /*
+         * Replaces the name's space characters with underscores and uses that
+         * as the id. 
+         */
         tempFish.id = tempFish.name.replace(/ /g, '_');
 
+        /*
+         * Retrieves the status for each hemisphere. 
+         */
+        tempFish.status =
+          MonthUtility.getStatusHemispheres(
+            new Date().getMonth(),
+            tempFish.months
+        );
+        
         this.setState({fish: tempFish});
       })
       .catch((error) => {
         console.error(error);
       });
   }
-
-  fishInfo () {
-    return <Fish fish={this.state.fish} key={this.state.fish.id} />
-  }
-  
 }
