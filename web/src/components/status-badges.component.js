1eb3d0023b33a924101589201bbc029d9cbc2b89
Alvin <22848944+Korywon@users.noreply.github.com>
2022-03-21 10:46:18 -0500

    Update status badge to use function syntax

--- web/src/components/status-badges.component.js
+++ web/src/components/status-badges.component.js
@@ -1,73 +1,63 @@
-import { Component } from 'react';
 import { Badge } from 'react-bootstrap';
 
 /**
  * This class is used to generate badges based on the status that is passed in.
  */
-export default class StatusBadges extends Component {
-  constructor (props) {
-    super(props);
+function StatusBadges({id, status}) {
+  let badges = [];
 
-    this.state = {
-      id: this.props.id,
-      status: this.props.status
-    };
+  /*
+    * Badges for new, available, soon, or unavailable.
+    */
+  if (status.new && new Date().getDay() < 8) {
+    badges.push(
+      <span key={id + '-statusBadge-new'}>
+        <Badge pill variant="primary">new</Badge>&nbsp;
+      </span>
+    );
+  } else if (status.available) {
+    badges.push(
+      <span key={id + '-statusBadge-available'}>
+        <Badge pill variant="success">available</Badge>&nbsp;
+      </span>
+    );
+  } else if(status.soon) {
+    badges.push(
+      <span key={id + '-statusBadge-info'}>
+        <Badge pill variant="info">next month</Badge>&nbsp;
+      </span>
+    );
+  } else {
+    badges.push(
+      <span key={id + '-statusBadge-unavailable'}>
+        <Badge pill variant="secondary">unavailable</Badge>&nbsp;
+      </span>
+    );
   }
 
-  render () {
-    let badges = [];
-  
-    /*
-     * Badges for new, available, soon, or unavailable.
-     */
-    if (this.state.status.new && new Date().getDay() < 8) {
-      badges.push(
-        <span key={this.state.id + '-statusBadge-new'}>
-          <Badge pill variant="primary">new</Badge>&nbsp;
-        </span>
-      );
-    } else if (this.state.status.available) {
-      badges.push(
-        <span key={this.state.id + '-statusBadge-available'}>
-          <Badge pill variant="success">available</Badge>&nbsp;
-        </span>
-      );
-    } else if(this.state.status.soon) {
-      badges.push(
-        <span key={this.state.id + '-statusBadge-info'}>
-          <Badge pill variant="info">next month</Badge>&nbsp;
-        </span>
-      );
-    } else {
-      badges.push(
-        <span key={this.state.id + '-statusBadge-unavailable'}>
-          <Badge pill variant="secondary">unavailable</Badge>&nbsp;
-        </span>
-      );
-    }
-  
-    /*
-     * If this is the last month to catch the fish. 
-     */
-    if (this.state.status.lastMonth) {
-      badges.push(
-        <span key={this.state.id + '-statusBadge-lastMonth'}>
-          <Badge pill variant="danger">last month</Badge>&nbsp;
-        </span>
-      );
-    }
-  
-    /*
-     * If this is the last hour to catch the fish. 
-     */
-    if (this.state.status.lastHour) {
-      badges.push(
-        <span key={this.state.id + 'statusBadge-lastHour'}>
-          <Badge pill variant="warning">last hour</Badge>&nbsp;
-        </span>
-      );
-    }
-    
-    return(badges);
+  /*
+    * If this is the last month to catch the fish.
+    */
+  if (status.lastMonth) {
+    badges.push(
+      <span key={id + '-statusBadge-lastMonth'}>
+        <Badge pill variant="danger">last month</Badge>&nbsp;
+      </span>
+    );
   }
+
+  /*
+    * If this is the last hour to catch the fish.
+    */
+  if (status.lastHour) {
+    badges.push(
+      <span key={id + 'statusBadge-lastHour'}>
+        <Badge pill variant="warning">last hour</Badge>&nbsp;
+      </span>
+    );
+  }
+
+  return(badges);
 }
+
+export default StatusBadges;
